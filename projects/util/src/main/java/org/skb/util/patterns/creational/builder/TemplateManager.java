bd1add973c3de01e4b4e0fc2e85a35c250bc2d17
Sven van der Meer <sven@vandermeer.de>
2011-09-09 14:47:20 +0100

    moving patterns into new structure

---
+++ projects/util/src/main/java/org/skb/util/patterns/creational/builder/TemplateManager.java
@@ -0,0 +1,66 @@
+/* Copyright (c) 2010-2011 Sven van der Meer
+ * All rights reserved.
+ *
+ * Redistribution  and  use  in  source  and  binary  forms,  with  or  without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     + Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     + Redistributions  in binary  form must  reproduce the  above copyright
+ *       notice, this list  of conditions and  the following disclaimer  in the
+ *     + Neither the name of the the author nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS  IS"
+ * AND ANY EXPRESS  OR IMPLIED WARRANTIES,  INCLUDING, BUT NOT  LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY  AND FITNESS FOR A  PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN  NO EVENT SHALL  THE COPYRIGHT HOLDER  OR CONTRIBUTORS BE
+ * LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,   OR
+ * CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT  LIMITED  TO,  PROCUREMENT  OF
+ * SUBSTITUTE GOODS  OR SERVICES;  LOSS OF  USE, DATA,  OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER  CAUSED AND  ON ANY  THEORY OF  LIABILITY, WHETHER  IN
+ * CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE  USE OF THIS SOFTWARE, EVEN IF ADVISED  OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * [The BSD License, http://www.opensource.org/licenses/bsd-license.php]
+ */
+
+package org.skb.util.pattern;
+
+import org.skb.util.types.atomic.stringtemplate.TSSTGManager;
+
+/**
+ * A class managing templates for a builder.
+ *  
+ * @author     Sven van der Meer <sven@vandermeer.de>
+ * @version    v1.0.0 build 110901 (01-Sep-11) with Java 1.6
+ */
+public interface TemplateManager {
+	/**
+	 * Indicates if the instance is initialised or not
+	 * @return true if initialised, false otherwise. Note: if return is false some functionality of the instance might not be available
+	 */
+	public boolean isInitialised();
+
+	//TODO clarify what name/key mean
+	/**
+	 * Adds a template with name and key
+	 * @param name name
+	 * @param key key
+	 */
+	public void add_template(String name, String key);
+
+	public void set_header();
+
+	public TSSTGManager get_template_object(String key);
+
+	/**
+	 * Returns the path (the fully qualified path name) for the template <code>key</code>
+	 * @param key key of the template
+	 * @return path to the template file
+	 */
+	public String get_template_path(String key);
+
+}
