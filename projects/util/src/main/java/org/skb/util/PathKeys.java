89c595da4b3d98ac310763475f96442e244e6bc7
Sven van der Meer <sven@vandermeer.de>
2011-09-13 10:50:51 +0100

    new classes maintaining all external field/path keys

---
+++ projects/util/src/main/java/org/skb/util/PathKeys.java
@@ -0,0 +1,21 @@
+package org.skb.util;
+
+public class PathKeys {
+	/** Key for a path that holds configuration information for the Tribe library */
+	public final static String pathTribeConfiguration		= "skb/tribe/configuration";
+
+	/** Key for a path that holds configuration information for a parser/compiler */
+	public final static String pathLangConfiguration		= "skb/lang/configuration";
+
+	/** Key for a path that holds information on ANTLR token */
+	public final static String pathLangTokens				= "skb/lang/tokens";
+
+	/** Key for a path that holds information about target languages */
+	public final static String pathLangTargets				= "skb/lang/targets";
+
+	/** Key for a path that holds information in StringTemplate chunks */
+	public final static String pathLangStgChunks			= "skb/lang/stgchunks";
+
+	/** Key for a path that holds information language rules */
+	public final static String pathLangRules				= "skb/lang/rules";
+}
